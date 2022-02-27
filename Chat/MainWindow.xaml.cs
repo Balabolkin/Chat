581e489209db31a6458e30a597af06fdf0573319
Andrey <hohlov.andrej2014@live.ru>
2022-02-27 21:40:15 +0700

    Добавьте файлы проекта.

---
+++ Chat/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinRT;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using WinRT.Interop;
+using Windows.UI.Popups;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Chat
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+
+    public interface IInitializeWithWindow
+    {
+        void Initialize(IntPtr hwnd);
+    }
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            
+        }
+        public enum SignInResult
+        {
+            SignInOK,
+            SignInFail,
+            SignInCancel,
+            Nothing
+        }
+        public sealed partial class signInDialog : ContentDialog
+        {
+            public SignInResult Result { get; set; }
+
+            private void ContentDialog_PrimaryButtonClick(object sender, RoutedEventArgs e)
+            {
+                this.Result = SignInResult.SignInOK;
+            }
+
+            private void ContentDialog_CloseButtonClick(object sender, RoutedEventArgs e)
+            {
+                this.Result = SignInResult.SignInCancel;
+            }
+        }
+        private async void syncButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            SignInContentDialog signInDialog = new SignInContentDialog();
+            signInDialog.XamlRoot = this.Content.XamlRoot;
+            await signInDialog.ShowAsync();
+            usernameBlock.Text = signInDialog.Result;
+        }
+
+        //private async void syncButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        using (StreamReader sr = new StreamReader(@"D:\YandexDisk\VKI\System programming\Lomakin\chat.txt"))
+        //        {
+        //            chatBox.Text = await sr.ReadToEndAsync();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        ContentDialog errDialog = new ContentDialog()
+        //        {
+        //            Title = "Error",
+        //            Content = ex.ToString(),
+        //            PrimaryButtonText = "Ok",
+        //        };
+        //        errDialog.XamlRoot = this.Content.XamlRoot;
+        //        await errDialog.ShowAsync();
+
+        //    }
+        //}
+    }
+}
---
+++ Chat/signInContentDialog.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Chat
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SignInContentDialog : ContentDialog
+    {
+        public string Result { get; private set; }
+
+        private bool SignInCheck()
+        {
+            //using (SqlConnection connection = new SqlConnection(@"Data Source = BALABOLKIN-DESK\SQLEXPRESS; Initial Catalog = LOGIN; Integrated Security = True;"))
+            //{
+            //        connection.Open();
+            //        SqlCommand command = new SqlCommand("SELECT * from [LoginUser] WHERE (UserLogin = @login) AND (UserPassword = @pass)", connection);
+            //        SqlParameter logPr = new SqlParameter("@login", userNameTextBox.Text);
+            //        command.Parameters.Add(logPr);
+            //        SqlParameter PassPr = new SqlParameter("@pass", passwordTextBox.Password);
+            //        command.Parameters.Add(PassPr);
+            //        SqlDataReader sqlReader = command.ExecuteReader();
+            //        if (sqlReader.HasRows == true)
+            //        {
+            //            return true;
+            //        }
+            //        else
+            //        {
+            //            return false;
+            //        }
+            //}
+            return true;
+        }
+
+        public SignInContentDialog()
+        {
+            this.InitializeComponent();
+        }
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            // Ensure the user name and password fields aren't empty. If a required field
+            // is empty, set args.Cancel = true to keep the dialog open.
+            if (string.IsNullOrEmpty(userNameTextBox.Text))
+            {
+                args.Cancel = true;
+            }
+            else if (string.IsNullOrEmpty(passwordTextBox.Password))
+            {
+                args.Cancel = true;
+            }
+
+            // If you're performing async operations in the button click handler,
+            // get a deferral before you await the operation. Then, complete the
+            // deferral when the async operation is complete.
+
+            ContentDialogButtonClickDeferral deferral = args.GetDeferral();
+            if (SignInCheck())
+            {
+                this.Result = "User";
+            }
+            else
+            {
+                this.Result = "Guest";
+            }
+            deferral.Complete();
+        }
+        private void ContentDialog_CloseButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            this.Result = "Guest";
+        }
+    }
+}
